6264c68f3ea81f15352832e8004856773d92f438
Jesse-Tingle <jtingle0@gmail.com>
2019-10-06 20:47:52 -0400

    created UserCard.js component and functionality

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import UserCard from './components/UserCard';
 
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <UserCard />
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+ /* 
+      User Card
+      step 1: Create component for my user card
+      step 2: Create axios get request to retrieve data for user card
+      step 3: Assign items from axios call to state
+      step 4: render component to screen
+       */
+
+      /* 
+      List of Followers
+      step 1: Create component for follower user card
+      step 2: Create axios get request to retrieve data for followers
+      step 3: map over data
+      step 4: Assign items from axios call to state
+      step 5: render component to screen  */
